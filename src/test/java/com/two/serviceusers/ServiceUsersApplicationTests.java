73bc4cdf0f18fc7e82002076b8a4816677eed283
gerryfletch <1997Gerry@gmail.com>
2019-09-01 20:53:32 +0100

    Initial commit with complete POM.

---
+++ src/test/java/com/two/serviceusers/ServiceUsersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.two.serviceusers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceUsersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
