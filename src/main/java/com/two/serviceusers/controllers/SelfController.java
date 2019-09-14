50e0fed49725a64843d29735035f38d60b804197
gerryfletch <1997Gerry@gmail.com>
2019-09-14 20:57:45 +0100

    Created shell with HTTP API to test.

---
+++ src/main/java/com/two/serviceusers/controllers/SelfController.java
@@ -0,0 +1,12 @@
+package com.two.serviceusers.controllers;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class SelfController {
+
+
+}
