577bd61c9852e817b0cee9c365a425495a607ca6
gerryfletch <1997Gerry@gmail.com>
2019-09-30 10:58:25 +0100

    Created service to store the user

---
+++ src/main/java/com/two/serviceusers/users/UserService.java
@@ -0,0 +1,25 @@
+package com.two.serviceusers.users;
+
+import com.two.http_api.model.Tokens;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    /**
+     * @return a pair of JSON web tokens.
+     */
+    public Tokens createUser(UserRegistration userRegistration) {
+        int uid = this.userDao.storeUser(userRegistration);
+        return this.userDao.storeCredentials(uid, userRegistration.getPassword());
+    }
+
+}
