82bee5f5af2dcc5efac29c12233f5ac0ea1c2d2a
gerryfletch <1997Gerry@gmail.com>
2019-10-01 00:32:09 +0100

    Wrote additional validation messages and included valid annotation

--- src/main/java/com/two/serviceusers/users/UserRegistration.java
+++ src/main/java/com/two/serviceusers/users/UserRegistration.java
@@ -6,15 +6,19 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @Value
 public class UserRegistration {
+    @NotEmpty(message = "Email must be provided.")
     @Email
     private final String email;
 
+    @NotEmpty(message = "Password must be provided.")
     @Length(min = 5)
     private final String password;
 
+    @NotEmpty(message = "Name must be provided.")
     @Length(min = 5)
     private final String name;
 
