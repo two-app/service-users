e4295715b0fb862b382191c0234a544bb79fcd88
gerryfletch <1997Gerry@gmail.com>
2019-09-30 20:10:48 +0100

    Implemented AllArgsConstructor for controller

--- src/main/java/com/two/serviceusers/self/SelfController.java
+++ src/main/java/com/two/serviceusers/self/SelfController.java
@@ -3,6 +3,7 @@ package com.two.serviceusers.self;
 import com.two.http_api.model.Tokens;
 import com.two.serviceusers.users.UserRegistration;
 import com.two.serviceusers.users.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,13 +13,10 @@ import javax.validation.constraints.NotNull;
 
 @RestController
 @Validated
+@AllArgsConstructor
 public class SelfController {
 
     private final UserService userService;
-
-    public SelfController(UserService userService) {
-        this.userService = userService;
-    }
 
     @PostMapping("/self")
     Tokens registerUser(@RequestBody @NotNull UserRegistration registerUser) {
