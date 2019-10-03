525e7ef1c10bce7b2aa95acf49ad3c010ba875a1
Gerry Fletcher <gerryfletch@users.noreply.github.com>
2019-10-03 19:10:41 +0100

    Dependencies and code for logging requests + JOOQ (#4)

--- src/main/java/com/two/serviceusers/ServiceUsersApplication.java
+++ src/main/java/com/two/serviceusers/ServiceUsersApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceUsersApplication {
 
     public static void main(String[] args) {
+        System.setProperty("org.jooq.no-logo", "true");
         SpringApplication.run(ServiceUsersApplication.class, args);
     }
 
