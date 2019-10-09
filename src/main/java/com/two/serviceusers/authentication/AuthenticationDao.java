83fa05b965426287d1c4c219678005559e4ded3c
gerryfletch <1997Gerry@gmail.com>
2019-10-09 22:42:40 +0100

    Moved Authentication DAO to propagate HTTP response exception.

--- src/main/java/com/two/serviceusers/authentication/AuthenticationDao.java
+++ src/main/java/com/two/serviceusers/authentication/AuthenticationDao.java
@@ -1,12 +1,14 @@
 package com.two.serviceusers.authentication;
 
 import com.two.http_api.api.AuthenticationServiceApi;
+import com.two.http_api.exceptions.PropagateHttpResponseException;
 import com.two.http_api.model.Tokens;
 import com.two.http_api.model.User;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Repository
 @AllArgsConstructor
@@ -17,19 +19,29 @@ public class AuthenticationDao {
 
     /**
      * @return JWTs from the Authentication Service if the storage was successful.
-     * TODO: Some error handling for WebClient ResponseException
      */
     public Tokens storeCredentials(User.WithCredentials cr) {
         logger.info("Messaging the Authentication Service to store the users credentials, and generate tokens.");
-        return this.authenticationServiceApi.storeCredentialsAndGenerateTokens(cr);
+        try {
+            return this.authenticationServiceApi.storeCredentialsAndGenerateTokens(cr);
+        } catch (WebClientResponseException e) {
+            logger.error("Failed to store user credentials or generate tokens.in authentication service.");
+            logger.error("Potential data-mismatch between users service and authentication service.");
+            logger.error("Response for UID {}.", cr.getUser().getUid(), e);
+            throw new PropagateHttpResponseException(e);
+        }
     }
 
     /**
      * @return JWTs from the Authentication Service if the authentication was successful.
-     * TODO: Some error handling for WebClient ResponseException
      */
     public Tokens authenticateAndCreateTokens(User.WithCredentials cr) {
         logger.info("Messaging the Authentication Service to authenticate UID {}, and generate tokens.", cr.getUser().getUid());
-        return this.authenticationServiceApi.authenticateCredentialsAndGenerateTokens(cr);
+        try {
+            return this.authenticationServiceApi.authenticateCredentialsAndGenerateTokens(cr);
+        } catch (WebClientResponseException e) {
+            logger.warn("Failed to authenticate user {}.", cr.getUser().getUid());
+            throw new PropagateHttpResponseException(e);
+        }
     }
 }
