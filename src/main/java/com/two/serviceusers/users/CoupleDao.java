c86f7ff4349a438ebabb4cfdea028ede678174de
gerryfletch <1997Gerry@gmail.com>
2019-10-12 16:43:53 +0100

    Added logging to couple dao

--- src/main/java/com/two/serviceusers/users/CoupleDao.java
+++ src/main/java/com/two/serviceusers/users/CoupleDao.java
@@ -3,6 +3,8 @@ package com.two.serviceusers.users;
 import lombok.AllArgsConstructor;
 import org.jooq.DSLContext;
 import org.jooq.generated.tables.records.CoupleRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import static org.jooq.generated.Tables.COUPLE;
@@ -12,11 +14,13 @@ import static org.jooq.generated.Tables.COUPLE;
 public class CoupleDao {
 
     private final DSLContext ctx;
+    private static final Logger logger = LoggerFactory.getLogger(CoupleDao.class);
 
     /**
      * @return the CID of the new relationship.
      */
     public int storeCouple(int uid, int pid) {
+        logger.info("Creating couple with UID {} and PID {}.", uid, pid);
         CoupleRecord coupleRecord = ctx.newRecord(COUPLE);
 
         coupleRecord.setUid(uid);
@@ -25,6 +29,7 @@ public class CoupleDao {
         coupleRecord.store();
         coupleRecord.refresh();
 
+        logger.info("Successfully stored couple and generated CID {}.", coupleRecord.getCid());
         return coupleRecord.getCid();
     }
 
