f24ac21febdac33e4131400dd27149476d5b1288
gerryfletch <1997Gerry@gmail.com>
2019-10-12 17:40:18 +0100

    Wrote method to connect a user to their partner.

--- src/main/java/com/two/serviceusers/users/CoupleDao.java
+++ src/main/java/com/two/serviceusers/users/CoupleDao.java
@@ -1,19 +1,23 @@
 package com.two.serviceusers.users;
 
+import com.two.http_api.model.User;
 import lombok.AllArgsConstructor;
 import org.jooq.DSLContext;
 import org.jooq.generated.tables.records.CoupleRecord;
+import org.jooq.generated.tables.records.UserRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import static org.jooq.generated.Tables.COUPLE;
+import static org.jooq.generated.Tables.USER;
 
 @Repository
 @AllArgsConstructor
 public class CoupleDao {
 
     private final DSLContext ctx;
+    private final UserMapper userMapper;
     private static final Logger logger = LoggerFactory.getLogger(CoupleDao.class);
 
     /**
@@ -31,6 +35,27 @@ public class CoupleDao {
 
         logger.info("Successfully stored couple and generated CID {}.", coupleRecord.getCid());
         return coupleRecord.getCid();
+    }
+
+    /**
+     * @param uid to update.
+     * @param pid to set in the uids record.
+     * @param cid to set in the uids record.
+     * @return a refreshed user object holding the new pid and cid values.
+     */
+    public User connectUserToPartner(int uid, int pid, int cid) {
+        logger.info("Retrieving user record for UID {}.", uid);
+        UserRecord userRecord = ctx.selectFrom(USER).where(USER.UID.eq(uid)).fetchOne();
+
+        logger.info("Setting PID to {} and CID to {}.", pid, cid);
+        userRecord.setPid(pid);
+        userRecord.setCid(cid);
+
+        userRecord.update();
+        userRecord.refresh();
+
+        logger.info("Successfully updated the user record.");
+        return userMapper.map(userRecord);
     }
 
 }
--- src/test/java/com/two/serviceusers/users/CoupleDaoTest.java
+++ src/test/java/com/two/serviceusers/users/CoupleDaoTest.java
@@ -1,5 +1,6 @@
 package com.two.serviceusers.users;
 
+import com.two.http_api.model.User;
 import org.flywaydb.core.Flyway;
 import org.jooq.DSLContext;
 import org.jooq.generated.tables.records.CoupleRecord;
@@ -15,10 +16,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.jooq.generated.Tables.COUPLE;
+import static org.jooq.generated.Tables.USER;
 
 @ExtendWith(SpringExtension.class)
 @JooqTest
@@ -28,12 +31,14 @@ class CoupleDaoTest {
     private final Flyway flyway;
     private final DSLContext ctx;
     private final CoupleDao coupleDao;
+    private final UserDao userDao;
 
     @Autowired
     CoupleDaoTest(Flyway flyway, DSLContext ctx) {
         this.flyway = flyway;
         this.ctx = ctx;
-        this.coupleDao = new CoupleDao(ctx);
+        this.coupleDao = new CoupleDao(ctx, new UserMapper());
+        this.userDao = new UserDao(ctx, new UserMapper());
     }
 
     @BeforeEach
@@ -83,6 +88,46 @@ class CoupleDaoTest {
             Instant oneSecondAfter = Instant.now().plusSeconds(1);
 
             assertThat(connectedAt).isBetween(oneSecondBefore, oneSecondAfter);
+        }
+    }
+
+    private int createUser(int uidx) {
+        return userDao.storeUser(
+                new UserRegistration("gerry" + uidx + "@two.com", "testPass", "Gerry", LocalDate.parse("1997-08-21"))
+        ).getUid();
+    }
+
+    @Nested
+    class SetUsersPartner {
+        @Test
+        @DisplayName("it should update the existing records pid and cid")
+        void updatesExistingRecord() {
+            int uid = createUser(1), pid = createUser(2);
+            int cid = coupleDao.storeCouple(uid, pid);
+
+            User user = coupleDao.connectUserToPartner(uid, pid, cid);
+            User partner = coupleDao.connectUserToPartner(pid, uid, cid);
+
+            assertThat(user.getPid()).isEqualTo(pid);
+            assertThat(user.getCid()).isEqualTo(cid);
+
+            assertThat(partner.getPid()).isEqualTo(uid);
+            assertThat(partner.getCid()).isEqualTo(cid);
+        }
+
+        @Test
+        @DisplayName("it should not create any additional rows, making the change in place")
+        void changeInPlace() {
+            int uid = createUser(1), pid = createUser(2);
+            int cid = coupleDao.storeCouple(uid, pid);
+            int previousNumOfUsers = ctx.selectCount().from(USER).fetchOne().value1();
+
+            // cause the update
+            coupleDao.connectUserToPartner(uid, pid, cid);
+            coupleDao.connectUserToPartner(pid, uid, cid);
+
+            int currentNumOfUsers = ctx.selectCount().from(USER).fetchOne().value1();
+            assertThat(previousNumOfUsers).isEqualTo(currentNumOfUsers);
         }
     }
 
