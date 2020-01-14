c0090e40717a5595adf8e7979c5c736c0983cf3f
gerryfletch <1997Gerry@gmail.com>
2020-01-14 22:36:25 +0000

    Removed todo

--- src/main/java/com/two/serviceusers/users/PartnerController.java
+++ src/main/java/com/two/serviceusers/users/PartnerController.java
@@ -3,7 +3,6 @@ package com.two.serviceusers.users;
 import com.two.http_api.api.PublicApiContracts;
 import com.two.http_api.authentication.RequestContext;
 import com.two.http_api.model.Partner;
-import com.two.http_api.model.User;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,16 +24,8 @@ public class PartnerController implements PublicApiContracts.GetPartner {
     @Override
     public Partner getPartner(HttpServletRequest req) {
         var ctx = RequestContext.from(req);
-        var pid = ctx.getPid();
-        logger.info("User {} retrieving partner with pid {}.", ctx.getUid(), pid);
-
-        if (pid == null) {
-            // TODO replace with a filter in Zuul for all routes requiring connection
-            logger.warn("User not connected with partner.");
-            throw new UserNotExistsException();
-        }
-
-        return userService.getUser(pid, Partner.class);
+        logger.info("User {} retrieving partner with pid {}.", ctx.getUid(), ctx.getPid());
+        return userService.getUser(ctx.getPid(), Partner.class);
     }
 }
 
